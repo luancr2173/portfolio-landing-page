2ad6b3e6640ffbac6c12e8efd99451921ed905fe
Luan Carvalho <luan.ramos.al@gmail.com>
2026-03-09 18:10:54 -0300

    app.jsx atualizado importação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import LandingApp from './pages/LandingApp';
 import LandingSaaS from './pages/LandingSaaS';
 import LandingStartup from './pages/LandingStartup';
