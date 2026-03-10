26adf0b61fffe7f4af8f82fa9d1b0d982bd99b25
Luan Carvalho <luan.ramos.al@gmail.com>
2026-03-09 23:34:49 -0300

    feat: Framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Home from './pages/Home';
 import LandingApp from './pages/LandingApp';
 import LandingSaaS from './pages/LandingSaaS';
@@ -8,16 +9,48 @@ import LandingAgency from './pages/LandingAgency';
 import LandingAI from './pages/LandingAI';
 import './App.css';
 
+const pageVariants = {
+  initial: {
+    opacity: 0
+  },
+  in: {
+    opacity: 1
+  },
+  out: {
+    opacity: 0
+  }
+};
+
+const pageTransition = {
+  duration: 0.5
+};
+
+const AnimatedRoute = ({ children }) => (
+  <motion.div
+    initial="initial"
+    animate="in"
+    exit="out"
+    variants={pageVariants}
+    transition={pageTransition}
+  >
+    {children}
+  </motion.div>
+);
+
 function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/app" element={<LandingApp />} />
-      <Route path="/saas" element={<LandingSaaS />} />
-      <Route path="/startup" element={<LandingStartup />} />
-      <Route path="/agency" element={<LandingAgency />} />
-      <Route path="/ai" element={<LandingAI />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<AnimatedRoute><Home /></AnimatedRoute>} />
+        <Route path="/app" element={<AnimatedRoute><LandingApp /></AnimatedRoute>} />
+        <Route path="/saas" element={<AnimatedRoute><LandingSaaS /></AnimatedRoute>} />
+        <Route path="/startup" element={<AnimatedRoute><LandingStartup /></AnimatedRoute>} />
+        <Route path="/agency" element={<AnimatedRoute><LandingAgency /></AnimatedRoute>} />
+        <Route path="/ai" element={<AnimatedRoute><LandingAI /></AnimatedRoute>} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
